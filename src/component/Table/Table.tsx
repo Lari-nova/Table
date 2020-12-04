f75af2a987731247ef8212e4ee4216834f033a19
Lari-nova <larinova13@gmail.com>
2020-12-04 17:04:05 +0300

    New table layout

--- src/component/Table/Table.tsx
+++ src/component/Table/Table.tsx
@@ -11,34 +11,99 @@ interface Props {
 }
 
 const Table = ({ data, onSort, sort, sortField, onRowSelect}: Props) => (
-	<table className="table">
-		<thead>
-		<tr>
-			<TH onClick={() => onSort("id")}>ID {sortField === "id" ? <small>{sort}</small> : null}</TH>
-			<TH onClick={() => onSort("firstName")}>First Name {sortField === "firstName" ? <small>{sort}</small> : null}</TH>
-			<TH onClick={() => onSort("lastName")}>Last Name {sortField === "lastName" ? <small>{sort}</small> : null}</TH>
-			<TH onClick={() => onSort("email")}>E-mail {sortField === "email" ? <small>{sort}</small> : null}</TH>
-			<TH onClick={() => onSort("phone")}>Phone {sortField === "phone" ? <small>{sort}</small> : null}</TH>
-		</tr>
-		</thead>
+	<TableGrid>
+		<THEAD>
+		<TR>
+			<TH className={"th"} onClick={() => onSort("id")}>ID {sortField === "id" ? <small>{sort}</small> : null}</TH>
+			<TH className={"th"} onClick={() => onSort("firstName")}>First Name {sortField === "firstName" ? <small>{sort}</small> : null}</TH>
+			<TH className={"th"} onClick={() => onSort("lastName")}>Last Name {sortField === "lastName" ? <small>{sort}</small> : null}</TH>
+			<TH className={"th"} onClick={() => onSort("email")}>E-mail {sortField === "email" ? <small>{sort}</small> : null}</TH>
+			<TH className={"th"} onClick={() => onSort("phone")}>Phone {sortField === "phone" ? <small>{sort}</small> : null}</TH>
+		</TR>
+		</THEAD>
 		<tbody>
 			{
 				data?.map((item: Person) => (
-					<tr key={item.id + item.phone} onClick={() => onRowSelect(item)}>
-						<td>{item.id}</td>
-						<td>{item.firstName}</td>
-						<td>{item.lastName}</td>
-						<td>{item.email}</td>
-						<td>{item.phone}</td>
-					</tr>
+					<TR key={item.id + item.phone} onClick={() => onRowSelect(item)}>
+						<TD className={'td'}>{item.id}</TD>
+						<TD className={'td'}>{item.firstName}</TD>
+						<TD className={'td'}>{item.lastName}</TD>
+						<TD className={'td'}>{item.email}</TD>
+						<TD className={'td'}>{item.phone}</TD>
+					</TR>
 				))
 			}
 		</tbody>
-	</table>
+	</TableGrid>
 );
 
-const TH = styled.th`
-	cursor: pointer;
+const TableGrid = styled.table`
+	width: 100%;
+  margin: 0;
+  padding: 0;
+  border-collapse: collapse;
+  border-spacing: 0;
 `;
+
+const THEAD = styled.thead`
+  visibility: hidden;
+  background: #fafafa;
+	@media screen and (min-width: 600px) {
+    visibility: visible;
+	}
+`
+
+const TR = styled.tr`
+	border: 1px solid #ddd;
+	border-bottom: 2px solid #ddd;
+	padding: 5px;
+	margin-bottom: 10px;
+	display: block;
+	@media screen and (min-width: 600px) {
+		display: table-row;
+		border-bottom-width: 1px;
+		margin-bottom: 0;
+	}
+	&:nth-child(even) {
+		background: #fafafa;
+	}
+`
+
+
+const TH = styled.th`
+	
+	padding: 10px;
+	cursor: pointer;
+	text-align: left;
+	text-transform: uppercase;
+	font-size: 11px;
+`;
+
+const TD = styled.td`
+	padding: 10px;
+	text-align: right;
+	display: block;
+	font-size: 13px;
+	border-bottom: 1px dotted #ddd;
+	&:last-child {
+		border-bottom: none;
+	}
+	@media screen and (min-width: 600px) {
+		display: table-cell;
+		text-align: left;
+		font-size: 14px;
+		border-bottom: none;
+	}
+	&:before {
+		content: attr(data-label);
+		float: left;
+		text-transform: uppercase;
+		font-weight: bold;
+		@media screen and (min-width: 600px) {
+			content: '';
+			display: none;
+		}
+	}
+`
 
 export default Table;
